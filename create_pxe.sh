b8a56df7924bb13615269d2594351326683f9988
sud0k1ller <54325660+sud0k1ller@users.noreply.github.com>
2020-10-27 09:17:09 +0100

    Update create_pxe.sh

--- create_pxe.sh
+++ create_pxe.sh
@@ -12,7 +12,19 @@ yum update &&
 yum install dhcp &&
 ## HERE COPY DHCPD CONFIG FILE ##
 yum install tftp-server &&
+
+## PREPARE BIOS BOOTLOADERS ##
+## SOURCE: https://docs.fedoraproject.org/en-US/fedora/rawhide/install-guide/advanced/Network_based_Installations/ 
 yum instal syslinux &&
+mkdir -p /var/lib/tftpboot/pxelinux.cfg &&
+cp /usr/share/syslinux/{pxelinux.0,menu.c32,vesamenu.c32,ldlinux.c32,libcom32.c32,libutil.c32} /var/lib/tftpboot/ &&
+
+## PREPARE UEFI BOOTLOADERS
+## Source: https://docs.fedoraproject.org/en-US/fedora/rawhide/install-guide/advanced/Network_based_Installations/ 
+yum install shim-x64 grub2-efi-x64 --installroot=/tmp/fedora --releasever 30 &&
+mkdir -p /var/lib/tftpboot/uefi &&
+cp /tmp/fedora/boot/efi/EFI/fedora/{shimx64.efi,grubx64.efi} /var/lib/tftpboot/uefi/
+
 
 
 ###############################
